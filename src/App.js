16298ba19f20b7b2f75939b5bd8e19cc1077dae7
Yulia Ratseburzhynska <bachkovska@gmail.com>
2022-07-20 19:31:06 +0300

    info about open-source was relocated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="WeatherApp">
       <Form />
+      <div><h6>This app was coded by Yulia Ratseburzhynska, and is <a href="https://github.com/UyliaRa/weather-app-yulia">open-sourced</a></h6></div>
     </div>
   );
 }
